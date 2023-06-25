98225472959832cd85d71899cda750c13b9028d3
campercontrol <dev@campercontrol.com>
2023-06-25 15:58:11 -0600

    se agrega buscador de parents para admin

--- app/crud/campers/parent_crud.py
+++ app/crud/campers/parent_crud.py
@@ -1,6 +1,6 @@
 from model.campers import Parent
 from schema.campers.parent_schema import ParentCreate, ParentModify
-from sqlalchemy import case
+from sqlalchemy import case, or_
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from utils.db import db_mapping_rows_to_dict
@@ -32,7 +32,8 @@ def create_new_parent(db, new_parent: ParentCreate):
         print(f"No se pudo guardar en la base de datos: {e}")
     return db_parent
 
-def create_new_parent_user_id(db, new_parent: ParentCreate, user_id:int):
+
+def create_new_parent_user_id(db, new_parent: ParentCreate, user_id: int):
     db_parent = None
     try:
         new_parent.user_id = user_id
@@ -62,9 +63,31 @@ def update_parent_by_id(db: Session, parent_id: int, modify_parent: ParentModify
 
 
 # def get_campers_from_parent
- 
-def delete_parent(db: Session, parent_id:int):
-    parent = db.query(Parent).filter(Parent.id==parent_id).first()
+
+
+def delete_parent(db: Session, parent_id: int):
+    parent = db.query(Parent).filter(Parent.id == parent_id).first()
     db.delete(parent)
     db.commit()
-    return {"status" : True}
+    return {"status": True}
+
+
+def search_parent_by_name(db: Session, search: str):
+    possible_parent = (
+        db.query(
+            Parent.id,
+            Parent.tutor_name,
+            Parent.tutor_lastname_father,
+            Parent.tutor_lastname_mother,
+        )
+        .filter(
+            or_(
+                Parent.tutor_name.ilike(r"%{}%".format(search)),
+                Parent.tutor_lastname_father.ilike(r"%{}%".format(search)),
+                Parent.tutor_lastname_mother.ilike(r"%{}%".format(search)),
+            )
+        )
+        .all()
+    )
+
+    return db_mapping_rows_to_dict(possible_parent)
