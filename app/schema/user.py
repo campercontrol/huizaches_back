f07109459a8a39ee2d44d5c610bbbb53bf1f389a
campercontrol <dev@campercontrol.com>
2023-10-26 18:02:51 -0600

    Se corrige cambio de contraseña y cambio de correo

--- app/schema/user.py
+++ app/schema/user.py
@@ -72,3 +72,10 @@ class UserChangePassword(BaseModel):
     access_token: str
     password: str
     password_confirm: str
+
+
+
+class UserChangeEmail(BaseModel):
+    access_token: str
+    email: str
+    email_confirm: str
