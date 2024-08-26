ee4f199527c52a75f749803b075e43eb222b4f00
diegoJuarezVelasco <diegoaw591@gmail.com>
2024-08-26 17:01:19 -0600

    Fix get_camper_medical_visits

--- app/crud/medical/camper_visit_crud.py
+++ app/crud/medical/camper_visit_crud.py
@@ -46,38 +46,31 @@ def camper_visit_triage_for_camp(db, camper_id: int, camp_id: int):
 #     return db_mapping_rows_to_dict(camper_visits)
 
 def camper_visit_for_camp(db, camper_id: int, camp_id: int):
-    initial_visit = aliased(MedicalCamperVisit)
-    tracing_visit = aliased(MedicalCamperVisit)
-    tracing_visit_query = (db.query(
-                tracing_visit.id.label("tracing_visit_id"),
-                tracing_visit.initial_visit_id.label("tracing_visit_initial_visit_id"),
-                tracing_visit.diagnostic.label("tracing_visit_diagnostic"),
-                tracing_visit.administered_medications.label("tracing_visit_administered_medication"),
-                tracing_visit.doctor.label("tracing_visit_doctor"),
-                tracing_visit.attention_date.label("tracing_visit_attention_date"),
-                tracing_visit.attention_time.label("tracing_visit_attention_time"),
-                tracing_visit.description.label("tracing_visit_description"),
-                tracing_visit.attention_time.label("tracing_visit_attention_time"),
-                Constant.value,
-                tracing_visit.medication_authorization.label("tracing_visit_medical_authorization"),
-                tracing_visit.event_description.label("tracing_visit_event_description"),
-                tracing_visit.camp_restriction.label("tracing_visit_camp_restriction"),
-                tracing_visit.medical_monitoring.label("tracing_visit_medical_monitoring"),
-                tracing_visit.comment.label("tracing_visit_comment"),
-                tracing_visit.medical_comment.label("tracing_visit_medical_comment"),
-                tracing_visit.send_in_email.label("tracing_visit_send_in_email"),
-                tracing_visit.already_sent.label("tracing_visit_already_sent"),
-                tracing_visit.camper_id.label("tracing_visit_camper_id"))
-                .select_from(initial_visit)
-                .join(tracing_visit, initial_visit.id == tracing_visit.initial_visit_id, isouter=True)
-                .join(Constant, initial_visit.triage == Constant.id)
-                .where(and_(initial_visit.initial_visit_id.is_(None), initial_visit.camper_id == camper_id, initial_visit.camp_id == camp_id)))
-    
-    initial_visit_query = (
-        db.query(MedicalCamperVisit, Constant.value)
+        
+    initial_visits_query = (
+        db.query(
+                MedicalCamperVisit.id,
+                MedicalCamperVisit.initial_visit_id,
+                MedicalCamperVisit.diagnostic,
+                MedicalCamperVisit.administered_medications,
+                MedicalCamperVisit.doctor,
+                MedicalCamperVisit.attention_date,
+                MedicalCamperVisit.attention_time,
+                MedicalCamperVisit.description,
+                MedicalCamperVisit.attention_time,
+                Constant.value.label("triage"),
+                MedicalCamperVisit.medication_authorization,
+                MedicalCamperVisit.event_description,
+                MedicalCamperVisit.camp_restriction,
+                MedicalCamperVisit.medical_monitoring,
+                MedicalCamperVisit.comment,               
+                MedicalCamperVisit.medical_comment,
+                MedicalCamperVisit.send_in_email,
+                MedicalCamperVisit.already_sent,
+                MedicalCamperVisit.camper_id)
         .select_from(MedicalCamperVisit)
         .join(Constant, Constant.id == MedicalCamperVisit.triage)
-        .filter(
+        .where(
             and_(
                 MedicalCamperVisit.camper_id == camper_id,
                 MedicalCamperVisit.initial_visit_id.is_(None),
@@ -85,17 +78,49 @@ def camper_visit_for_camp(db, camper_id: int, camp_id: int):
             )
         )
     )
-    data_tracing_visit = db.execute(tracing_visit_query)
-    data_initial_visit = db.execute(initial_visit_query)
+    data_initial_visits = db.execute(initial_visits_query)
+    data_initial_visits = data_initial_visits.mappings().all()
     
-    data_tracing_visit = data_tracing_visit.mappings().all() 
-    data_initial_visit = data_initial_visit.mappings().first()
-
-    if(data_initial_visit):
+    camper_all_visits = []
+    
+    for data_initial_visit in data_initial_visits:    
+        tracing_visit_query = (db.query(
+            MedicalCamperVisit.id,
+            MedicalCamperVisit.initial_visit_id,
+            MedicalCamperVisit.diagnostic,
+            MedicalCamperVisit.administered_medications,
+            MedicalCamperVisit.doctor,
+            MedicalCamperVisit.attention_date,
+            MedicalCamperVisit.attention_time,
+            MedicalCamperVisit.description,
+            MedicalCamperVisit.attention_time,
+            Constant.value.label("triage"),
+            MedicalCamperVisit.medication_authorization,
+            MedicalCamperVisit.event_description,
+            MedicalCamperVisit.camp_restriction,
+            MedicalCamperVisit.medical_monitoring,
+            MedicalCamperVisit.comment,
+            MedicalCamperVisit.medical_comment,
+            MedicalCamperVisit.send_in_email,
+            MedicalCamperVisit.already_sent,
+            MedicalCamperVisit.camper_id)
+            .select_from(MedicalCamperVisit)
+            .join(Constant, Constant.id == MedicalCamperVisit.triage)
+            .where(
+                and_(
+                    MedicalCamperVisit.camper_id == camper_id,
+                    MedicalCamperVisit.initial_visit_id == data_initial_visit.id, 
+                    MedicalCamperVisit.camp_id == camp_id
+                )
+            )).order_by(MedicalCamperVisit.attention_date)   
+        data_tracing_visit = db.execute(tracing_visit_query)
+        data_tracing_visit = data_tracing_visit.mappings().all() 
+        
         initial_visit_dict = dict(data_initial_visit)
-        initial_visit_dict["tracing_visits"] = data_tracing_visit
-        return initial_visit_dict
-    return data_initial_visit
+        initial_visit_dict["tracing_visits"] = data_tracing_visit  
+        camper_all_visits.append(initial_visit_dict)
+    
+    return camper_all_visits
 
 def create_new_camper_visit(db: Session, new_camper_visit: CamperVisitCreate):
     db_camper_visit = None
