4ad74a9afee6fc51fce6a721b577d1f99ea39f34
diegoJuarezVelasco <diegoaw591@gmail.com>
2024-09-20 19:22:52 -0600

    Add mercadopago models

---
+++ app/model/mercadopago/mercadopago_merchant_order.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer
+)
+from utils.db import Base
+class MercadopagoMerchantOrder(Base):
+    __tablename__ = "mercadopago_merchant_order"
+
+    id = Column("id", Integer(), primary_key=True, nullable=False, autoincrement=True)
+    camp_id = Column(
+        ForeignKey("camps_camp.id", ondelete="cascade"), nullable=False, doc="Campamento"
+    )
+    camper_id = Column(
+        ForeignKey("campers_camper.id", ondelete="cascade"), nullable=False, doc='Camper'
+    )
+    merchant_order_id = Column(Integer, nullable=False)
+    created_at = Column("created", DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(
+        "updated",
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
---
+++ app/model/mercadopago/mercadopago_payment.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer
+)
+from utils.db import Base
+class MercadopagoPayment(Base):
+    __tablename__ = "mercadopago_payment"
+
+    id = Column("id", Integer(), primary_key=True, nullable=False, autoincrement=True)
+    camp_id = Column(
+        ForeignKey("camps_camp.id", ondelete="cascade"), nullable=False, doc="Campamento"
+    )
+    camper_id = Column(
+        ForeignKey("campers_camper.id", ondelete="cascade"), nullable=False, doc='Camper'
+    )
+    payment_id = Column(Integer, nullable=False)
+    created_at = Column("created", DateTime(timezone=True), default=datetime.utcnow)
+    updated_at = Column(
+        "updated",
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
