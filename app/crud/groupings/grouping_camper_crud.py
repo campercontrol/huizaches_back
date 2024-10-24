a7e67c9817d4fae25fb8cd3a83b463bb93ffc3d0
diegoJuarezVelasco <diegoaw591@gmail.com>
2024-10-24 00:56:30 -0600

    Validation added to add camper to grouping: camper cannot be in two groupings of the same type

--- app/crud/groupings/grouping_camper_crud.py
+++ app/crud/groupings/grouping_camper_crud.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session, aliased
+from sqlalchemy import and_
 from model.groupings.grouping_camper import GroupingCamper
 from model.groupings.grouping_camp import GroupingCamp
+from model.groupings.grouping_type import GroupingType
+from model.groupings.grouping import Grouping
 from model.campers.camper import Camper
 from model.catalogs.constant import Constant
 from model.camps import CamperInCamp
@@ -22,23 +25,42 @@ def get_grouping_camper_by_id(db: Session, grouping_camper_id: int):
 
 
 def create_new_grouping_camper(db: Session, grouping_campers: GroupingCamperCreate):
-    db.begin()
     grouping_camp_id = grouping_campers[0].grouping_camp_id
 
     try:
-        grouping_camp_row = db.query(GroupingCamp).filter(GroupingCamp.id == grouping_camp_id).first()
+        grouping_camp_row = (
+            db.query(GroupingCamp.id.label('grouping_id'), GroupingCamp.camp_id, GroupingType.id.label('grouping_type_id'), GroupingCamp.maximum_capacity).select_from(GroupingCamp).filter(GroupingCamp.id == grouping_camp_id)
+            .join(Grouping, Grouping.id == GroupingCamp.grouping_id)
+            .join(GroupingType, GroupingType.id == Grouping.grouping_type_id)
+            .first()
+        )
         grouping_current_capacity = db.query(GroupingCamper).filter(GroupingCamper.grouping_camp_id == grouping_camp_id).count()
 
         if len(grouping_campers) + grouping_current_capacity > grouping_camp_row.maximum_capacity:
-            return {"detail": "La cantidad de campers excede la capacidad de la agrupación"}
-
-        if grouping_camp_row == None:
-            return {"detail": "No se econtró el grouping_camp_id"}        
-
+            return {"status": 2, "detail": "La cantidad de campers excede la capacidad de la agrupación"}
+        
         for grouping_camper in grouping_campers:
-            search_grouping_camper = db.query(GroupingCamper).filter(GroupingCamper.camper_id == grouping_camper.camper_id, GroupingCamper.grouping_camp_id == grouping_camp_id).first()        
+            search_grouping_camper = (
+                db.query(GroupingCamper).select_from(GroupingCamper)
+                .filter(GroupingCamper.camper_id == grouping_camper.camper_id, GroupingCamper.grouping_camp_id == grouping_camp_id).first()
+                )
+            
+            camper_info = db.query(Camper.id, Camper.name, Camper.lastname_father, Camper.lastname_mother).filter(Camper.id == grouping_camper.camper_id).first()
+            camper_groupings = (
+                db.query(GroupingType.id.label("grouping_type_id"), GroupingCamp.id).select_from(GroupingCamper)
+                .join(GroupingCamp, GroupingCamp.id == GroupingCamper.grouping_camp_id)
+                .join(Grouping, Grouping.id == GroupingCamp.grouping_id)
+                .join(GroupingType, GroupingType.id == Grouping.grouping_type_id)                     
+            ).where(and_(GroupingCamper.camper_id == grouping_camper.camper_id, GroupingCamp.camp_id == grouping_camp_row.camp_id)).all()
+            
             if not search_grouping_camper == None:
-                return {"detail": "El camper " + str(search_grouping_camper.camper_id) + ' ya se encuentra en la agrupación. No se añadieron los campers'}
+                
+                return {"status":3, "detail": "El camper " + camper_info.name + " " + camper_info.lastname_father + " " + camper_info.lastname_mother + ' ya se encuentra en la agrupación. No se añadieron los campers.'}
+
+            for camper_grouping in camper_groupings:
+                if camper_grouping.grouping_type_id == grouping_camp_row.grouping_type_id:
+                    return {"status": 4, "detail": "El camper " + camper_info.name + " " + camper_info.lastname_father + " " + camper_info.lastname_mother + " ya se encuentra en una agrupación del mismo tipo, no es posible agregar."}
+            
             new_grouping_camper = GroupingCamper(**grouping_camper.dict())
             db.add(new_grouping_camper)
         db.commit()
@@ -47,8 +69,7 @@ def create_new_grouping_camper(db: Session, grouping_campers: GroupingCamperCrea
         db.rollback()
         raise HTTPException(status_code=500, detail="Ocurrio un error, los campers no se añadieron correctamente")
     
-    db.close()
-    return {"detail": "Se han añadido correctamente los campers a la agrupación"}
+    return {"status": 1, "detail": "Se han añadido correctamente los campers a la agrupación"}
     
 
 def assign_grouping_camp_to_campers(db: Session, campers_id, grouping_camp_id):
