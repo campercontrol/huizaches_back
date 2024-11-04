e5a2f4de1bbced82527fd65309c9680500848527
diegoJuarezVelasco <diegoaw591@gmail.com>
2024-11-04 10:34:22 -0600

    Fix camper wristband 1x11

--- app/utils/pdf/bracelet.py
+++ app/utils/pdf/bracelet.py
@@ -16,7 +16,6 @@ def generar_pdf_bracelete(list_campers, camp_name):
         code =  str(getattr(camper, "camper_id")).zfill(6)
         ruta1 = generate_code128_no_fotter(code) 
         barcode_64 = "data:image/png;base64," + img_to_base_64(f"{ruta1}").decode("utf-8")
-        
         context = {
             "name":  getattr(camper, "name"),
             "school": getattr(camper, "school"),
@@ -59,30 +58,26 @@ def generar_pdf_bracelete(list_campers, camp_name):
     html_content = template.render(list_context) #Renderiza los datos en el html
 
     print("//////////")
-    print(html_content)
+    # print(html_content)
 
     #Try para verificar paquete de libreria que permite usar la generacion de html a pdf 
-    try:
-        config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
-        output_pdf = "media/bracelete.pdf" #Nombre y ubicacion del archivo ya en pdf
-        pdfkit.from_string(
-            html_content, 
-            output_pdf, 
-            configuration=config,
-            options = {
-                'page-height': '11in',
-                'page-width': '1in',
-                'margin-top': '0',
-                'margin-right': '0',
-                'margin-bottom': '0',
-                'margin-left': '0',
-                'encoding': "UTF-8"
-                }
-            )
-         
-    except OSError:
-    #not present in PATH
-        print("path_not_found")
+    
+    output_pdf = "media/bracelete.pdf" #Nombre y ubicacion del archivo ya en pdf
+    config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
+    pdfkit.from_string(
+        html_content, 
+        output_pdf, 
+        configuration=config,
+        options = {
+            'page-height': '11in',
+            'page-width': '1in',
+            'margin-top': '0',
+            'margin-right': '0',
+            'margin-bottom': '0',
+            'margin-left': '0',
+            'encoding': "UTF-8"
+            }
+        )
     return output_pdf
 
 
