4b3aa106e80120051d9c0bdf6c73113947a5833a
diegoJuarezVelasco <diegoaw591@gmail.com>
2025-01-16 18:47:43 -0600

    Add camp staff available

--- app/model/camps/__init__.py
+++ app/model/camps/__init__.py
@@ -10,3 +10,4 @@ from model.camps.staff_in_camp import StaffInCamp
 from model.camps.camp_discount import CampDiscount
 from model.camps.tribe import Tribe
 from model.camps.camp_staff import CampStaff
+from model.camps.camp_staff_available import CampStaffAvailable
