ccc594de20e715d4f6df9b0516caf744679f2bb7
diegoJuarezVelasco <diegoaw591@gmail.com>
2025-01-19 01:55:06 -0600

    Add tribecamper table

--- app/model/camps/__init__.py
+++ app/model/camps/__init__.py
@@ -12,3 +12,4 @@ from model.camps.tribe import Tribe
 from model.camps.camp_staff import CampStaff
 from model.camps.camp_staff_available import CampStaffAvailable
 from model.camps.tribe_camp import TribeCamp
+from model.camps.tribe_camper import TribeCamper
