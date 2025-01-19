4e5961e7cf723e13780b3f38ac7af9db53b43541
diegoJuarezVelasco <diegoaw591@gmail.com>
2025-01-19 14:04:07 -0600

    Add camp_campaign table

--- app/model/mailings/__init__.py
+++ app/model/mailings/__init__.py
@@ -3,3 +3,4 @@ from model.mailings.campaign import Campaign
 from model.mailings.camper_campaign import CamperCampaign
 from model.mailings.school_campaign import SchoolCampaign
 from model.mailings.staff_campaign import StaffCampaign
+from model.mailings.camp_campaign import CampCampaign
