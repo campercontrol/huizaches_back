507b3da1c83e47459fe5fbe8ed61a265e296fdc2
diegoJuarezVelasco <diegoaw591@gmail.com>
2025-01-19 21:57:55 -0600

    Fix campaign parent model

--- app/model/mailings/parent_campaign.py
+++ app/model/mailings/parent_campaign.py
@@ -1,19 +1,19 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, SmallInteger, Float, Text, Date
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.schema import UniqueConstraint
 
 from utils.db import Base
 class ParentCampaign(Base):
-    __tablename__ = 'mailing_camp_campaign'
+    __tablename__ = 'mailing_parent_campaign'
     __table_args__ = (
         UniqueConstraint('parent_id', 'campaign_id'),
     )
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    campaign_id = Column(ForeignKey("mailing_campaign.id", nullable=False, ondelete="cascade"), doc="Campaña de mail")
+    campaign_id = Column(ForeignKey("mailing_campaign.id", ondelete="cascade"), nullable=False, doc="Campaña de mail")
     parent_id = Column(ForeignKey("campers_parent.id"), nullable=False, doc="Id del padre")
 
     created_at = Column("created", DateTime(timezone=True), default=datetime.utcnow)
