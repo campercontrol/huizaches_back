c4fc11a3860b524c1e6f6e1197ebf7d55ef4db58
diegoJuarezVelasco <diegoaw591@gmail.com>
2025-06-19 12:38:41 -0600

    Temporal token fix

--- app/main.py
+++ app/main.py
@@ -110,8 +110,9 @@ app.include_router(medical_routes)
 
 from service.role import role_routes
 from service.user import user_routes
-# from service.token import token_routes
-from service.auth.auth_service import token_routes
+from service.token import token_routes
+# Temporal Token Fix, please uncomment this code in the next iteration
+# from service.auth.auth_service import token_routes
 from service.image import image_routes
 from service.permission import permission_routes
 from service.generar_pdf import pdf_routes
