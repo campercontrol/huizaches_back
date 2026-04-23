600273dd06995081c16a69a74fad68f019ee80b4
diegoJuarezVelasco <diegoaw591@gmail.com>
2026-04-23 10:46:14 -0600

    update braceletes info and logos

--- app/utils/email_tools.py
+++ app/utils/email_tools.py
@@ -3,11 +3,11 @@ import requests
 import json
 import os
 
-domain_name = os.getenv("DOMAIN_NAME", "kincamp.com")
+domain_name = os.getenv("DOMAIN_NAME", "cc.camploshuizaches.mx")
 MAILGUN_SENDING_KEY = os.getenv("MAILGUN_SENDING_KEY")
 
 
-from_user_email = "Kin Camp <sistemas@kincamp.com>"
+from_user_email = "Los Huizaches Camp <info@cc.camploshuizaches.mx>"
 to_user_email = ""
 
 
